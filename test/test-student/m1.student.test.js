b1dedc78094297345b0894609cfc896fe68fac66
brown-cs1380 <cs1380@brown>
2025-01-29 20:57:35 +0000

    Update from https://github.com/brown-cs1380/project/commit/bede6613314acdd967ff915e9e4b9389c88e0846

---
+++ test/test-student/m1.student.test.js
@@ -0,0 +1,36 @@
+/*
+    In this file, add your own test cases that correspond to functionality introduced for each milestone.
+    You should fill out each test case so it adequately tests the functionality you implemented.
+    You are left to decide what the complexity of each test case should be, but trivial test cases that abuse this flexibility might be subject to deductions.
+
+    Imporant: Do not modify any of the test headers (i.e., the test('header', ...) part). Doing so will result in grading penalties.
+*/
+
+const distribution = require('../../config.js');
+
+test('(1 pts) student test', () => {
+  // Fill out this test case...
+    throw new Error('Not implemented');
+});
+
+
+test('(1 pts) student test', () => {
+  // Fill out this test case...
+    throw new Error('Not implemented');
+});
+
+
+test('(1 pts) student test', () => {
+  // Fill out this test case...
+    throw new Error('Not implemented');
+});
+
+test('(1 pts) student test', () => {
+  // Fill out this test case...
+    throw new Error('Not implemented');
+});
+
+test('(1 pts) student test', () => {
+  // Fill out this test case...
+    throw new Error('Not implemented');
+});
